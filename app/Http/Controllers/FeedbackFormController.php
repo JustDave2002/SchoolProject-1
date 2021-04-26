31877aa7a591b57203915d11f9f6425a5313e522
Dave Elshout <elsh0009@hz.nl>
2021-04-26 15:04:26 +0200

    bugfix
    Co-Authored-By: stef108 <70521492+stef108@users.noreply.github.com>

--- app/Http/Controllers/FeedbackFormController.php
+++ app/Http/Controllers/FeedbackFormController.php
@@ -43,7 +43,6 @@ class FeedbackFormController extends Controller
         $user_id = Auth::user()->id;
         $request->request->add(['user_id' => $user_id]);
         $this->validateFeedbackForm($request);
-        dd($request->all());
 
         $form = FeedbackForm::create([
             'user_id' => request('user_id'),
@@ -56,7 +55,6 @@ class FeedbackFormController extends Controller
           'question' => $q
         ]);
     }
-        dd($request->all());
 
         return redirect('feedbackForm');
     }
