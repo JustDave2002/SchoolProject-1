73083bf22c293785026aedea19cf79ba7c7f01bb
FilipCichoracki <70520204+FilipCichoracki@users.noreply.github.com>
2021-04-29 12:23:32 +0200

    Bugfixes
    Fixed Logo
    Fixed Edit Account page
    Fixed Name of guest

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-{{ __('New Feedback Form') }}
+{{ __('My Account') }}
 </h2>
 </x-slot>
 
@@ -10,15 +10,18 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 bg-white border-b border-gray-200">
-                <div>{{$user->name}}</div>
-                <div>{{$user->email}}</div>
+                <p><b>Name : </b>{{$user->name}}</p>
+                <p><b>Email : </b>{{$user->email}}</p>
                 @if($user->role_id == NULL)
-                    <div>Not specified</div>
+                    <p><b>Role : </b>Not specified</p>
                 @else
-                <div>{{$user->role->name}}</div>
+                    <p><b>Role : </b>{{$user->role->name}}</p>
                 @endif
-
-                <button onclick="location.href='/user/{{$user->id}}/edit'" type="button" style="float: left">Edit</button>
+                <br>
+                <br>
+                <br>
+                <br>
+                <button onclick="location.href='/user/{{$user->id}}/edit'" type="button" style="float: left"><b><i>Edit</i></b></button>
             </div>
         </div>
     </div>
