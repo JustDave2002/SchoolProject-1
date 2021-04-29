509357f1fa28f9e79e6697ce7d138993bee57472
Dave Elshout <elsh0009@hz.nl>
2021-04-29 12:17:27 +0200

    added p[ossibility for dark mode `
    Co-Authored-By: FilipCichoracki <70520204+FilipCichoracki@users.noreply.github.com>

--- resources/views/feedbackForm/index.blade.php
+++ resources/views/feedbackForm/index.blade.php
@@ -2,15 +2,16 @@
     <x-slot name="header">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.min.js"></script>
+
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-12 bg-gray-100 dark:bg-gray-900">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+                <div class="p-6 bg-white dark:bg-gray-800 border-b border-gray-200 ">
                 @if(session()->has('message'))
                         <div class="alert alert-success">
                             {{ session()->get('message') }}
