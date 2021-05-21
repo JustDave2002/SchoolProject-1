424ff4f2748f855fa49297d791d0f135063afe40
ThomasMelis <Meli0033@hz.nl>
2021-05-21 11:07:44 +0200

    made a mail template

--- resources/views/emails/mail.blade.php
+++ resources/views/emails/mail.blade.php
@@ -1,3 +1,14 @@
-Hello <strong>Rohan</strong>,
-<p>Wassap, bitch</p>
-<img src="https://pbs.twimg.com/media/Dh52vVmXUAAS3uI?format=png&name=small" alt="">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+      integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
+      crossorigin="anonymous">
+<h1>Hello!</h1>
+<img src="https://i.imgur.com/mXPaedC.png" width="70" , height="70">
+<H2>Do you want to give me feedback?</H2>
+<button style="
+    padding: 0.5em 1.5em;
+    border: none;
+    border-radius: 0.15em;
+    color: white;
+    background-color: #3b82f6;
+    " onclick="">Give Feedback
+</button>
