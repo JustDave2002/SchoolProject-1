96fab5b82d82e52409f50af39e6cec2b75f45143
Dave Elshout <elsh0009@hz.nl>
2021-05-23 03:23:13 +0200

    fixed bugs from merge

--- resources/views/feedbackForm/show.blade.php
+++ resources/views/feedbackForm/show.blade.php
@@ -18,12 +18,12 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             @if(Auth::user()->id == $binder->user_id)
                 {{ $feedbackForms->links() }}
-            <br>
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-
-                    @foreach($feedbackForms as $feedbackForm)
-                        <h1>{{$feedbackForm->title}}</h1>
+                <br>
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        @foreach($feedbackForms as $feedbackForm)
+                            <h1>{{$feedbackForm->title}}</h1>
+                        @endforeach
                         <br>
                         <!-- PDF button -->
                         <x-button class="ml-3" onclick="getPDF()">
@@ -33,7 +33,8 @@
                         <x-button class="ml-3" onclick="showElement()">
                             ask feedback
                         </x-button>
-                        <form class="formEmail" name="yes" style="visibility: hidden; padding-top: 20px; padding-left: 16px" action="/sendmail/test">
+                        <form class="formEmail" name="yes"
+                              style="visibility: hidden; padding-top: 20px; padding-left: 16px" action="/sendmail/test">
                             <div class="form-row align-items-center">
                                 <div class="col-auto">
                                     <label class="sr-only" for="inlineFormInput">E-mail</label>
@@ -52,18 +53,28 @@
                                     <button type="submit" class="btn btn-primary mb-2">Send</button>
                                 </div>
                             </div>
+                        </form>
+
+
+                        <!-- PDF section (everything in here will be in the PDF) -->
+                        <div class="canvas_div_pdf">
+                            <div class="container">
+                                <canvas id="myChart" width="1500" height="1000"></canvas>
+                            </div>
                         </div>
 
-
+                        <!-- table with answer information -->
                         <table class="table">
                             <thead>
                             <tr>
                                 <th scope="col">Questions</th>
                                 @foreach($feedbackForm->answerForms as $answerForm)
                                     @if($answerForm->guest == NULL)
-                                        <th scope="col">{{$answerForm->user->name}}, {{$answerForm->user->role->name}} </th>
+                                        <th scope="col">{{$answerForm->user->name}}
+                                            , {{$answerForm->user->role->name}} </th>
                                     @else
-                                        <th scope="col">{{$answerForm->guest->Name}}, {{$answerForm->guest->role->name}} </th>
+                                        <th scope="col">{{$answerForm->guest->Name}}
+                                            , {{$answerForm->guest->role->name}} </th>
                                     @endif
                                 @endforeach
                             </tr>
@@ -80,166 +91,22 @@
                             @endforeach
                             </tbody>
                         </table>
-
-                        <button ><a href="/guestAnswer/info/{{$binder->id}}">submit feedback</a></button>
-                        <button ><a href="/answer/create/{{$binder->id}}">submit feedback</a></button>
-                        @endforeach
-                    @else
-                        You don't have permission to view this Form.
-                    @endif
-                </div>
-            </div>
-            <!-- Script for making the Chart.js -->
-            <script>
-                let color = ['rgba(255, 0, 0, 0.4)', 'rgba(0, 0, 255, 0.4)', 'rgba(0, 204, 255, 0.4)', 'rgba(204, 102, 255, 0.4)', 'rgba(128, 0, 128, 0.4)'];
-                let counter = 0;
-                let myChart = document.getElementById('myChart').getContext('2d');
-
-                const data = {
-                    labels: [@foreach($feedbackForm->questions as $question)
-                        '{{$question->question}}',
-                        @endforeach],
-                    datasets: [
-                            @foreach($feedbackForm->answerForms as $answerForm){
-                            @if($answerForm->guest == NULL)
-                            label: '{{$answerForm->user->role->name}}',
-                            @else
-                            label: '{{$answerForm->guest->role->name}}',
-                            @endif
-                            data: [
-                                @foreach($answerForm->answers as $answer)
-                                    '{{$answer->answer}}',
-                                @endforeach
-                            ],
-                            borderColor: '#000',
-                            backgroundColor: `${color[counter++]}`,
-                            borderWidth: 1
-                        },
-                        @endforeach
-                    ]
-                };
-
-                // Global Options
-                Chart.defaults.global.defaultFontFamily = 'Arial';
-                Chart.defaults.global.defaultFontSize = 10;
-                Chart.defaults.global.defaultFontColor = 'black';
-
-                const options = {
-                    scale: {
-                        ticks: {
-                            min: 0,
-                            max: 5,
-                            stepSize: 1
-                        }
-                    },
-                    legend: {
-                        display: true,
-                        position: 'bottom',
-                        labels: {
-                            fontColor: '#000',
-                            fontSize: 12
-                        }
-                    },
-                    layout: {
-                        padding: {
-                            left: 0,
-                            right: 0,
-                            bottom: 0,
-                            top: 0
-                        },
-                    }
-                };
-
-
-                let massPopChart = new Chart(myChart, {
-                    type: 'radar',
-                    options: options,
-                    data: data,
-                });
-            </script>
-
-            <!-- Script for making the PDF download -->
-            <script>
-                function getPDF() {
-                    var HTML_Width = $(".canvas_div_pdf").width();
-                    var HTML_Height = $(".canvas_div_pdf").height();
-                    var top_left_margin = 15;
-                    var PDF_Width = HTML_Width + (top_left_margin * 2);
-                    var PDF_Height = (PDF_Width * 1.5) + (top_left_margin * 2);
-                    var canvas_image_width = HTML_Width;
-                    var canvas_image_height = HTML_Height;
-
-                    var totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
-
-
-                    html2canvas($(".canvas_div_pdf")[0], {allowTaint: true}).then(function (canvas) {
-                        canvas.getContext('2d');
-
-                        // console.log(canvas.height+"  "+canvas.width);
-
-                        var imgData = canvas.toDataURL("image/jpeg", 1.0);
-                        var pdf = new jsPDF('p', 'pt', [PDF_Width, PDF_Height]);
-                        pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin, canvas_image_width, canvas_image_height);
-
-                        for (var i = 1; i <= totalPDFPages; i++) {
-                            pdf.addPage(PDF_Width, PDF_Height);
-                            pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height * i) + (top_left_margin * 4), canvas_image_width, canvas_image_height);
-                        }
-
-                        pdf.save("{{$feedbackForm->title}}.pdf");
-                    });
-                };
-            </script>
-
-            <!-- Script to make email section visible -->
-            <script type="text/javascript">
-                function showElement() {
-                    element = document.querySelector('.formEmail');
-                    element.style.visibility = 'visible';
-                }
-            </script>
-            <!-- table with answer information -->
-            <table class="table">
-                <thead>
-                <tr>
-                    <th scope="col">Questions</th>
-                    @foreach($feedbackForm->answerForms as $answerForm)
-                        @if($answerForm->guest == NULL)
-                            <th scope="col">{{$answerForm->user->name}}
-                                , {{$answerForm->user->role->name}} </th>
+                            <button ><a href="/guestAnswer/info/{{$binder->id}}">submit feedback</a></button>
                         @else
-                            <th scope="col">{{$answerForm->guest->Name}}
-                                , {{$answerForm->guest->role->name}} </th>
+                            You don't have permission to view this Form.
                         @endif
-                    @endforeach
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($feedbackForm->questions as $question)
-                    <div></div>
-                    <tr>
-                        <th scope="row">{{$question->question}}</th>
-                        @foreach($question->answers as $answer)
-                            <td>{{$answer->answer}}</td>
-                        @endforeach
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
-
-            <button><a href="/answer/create/{{$feedbackForm->id}}">submit feedback</a></button>
-            @else
-                You don't have permission to view this Form.
-            @endif
+                    </div>
+                </div>
         </div>
     </div>
     </div>
     </div>
 </x-app-layout>
 
+
 <!-- Script for making the Chart.js -->
 <script>
-    let color = ['rgba(255, 99, 132, 0.6)', 'rgba(75, 192, 192, 0.6)', 'rgba(54, 162, 235, 0.6)'] ;
+    let color = ['rgba(255, 0, 0, 0.4)', 'rgba(0, 0, 255, 0.4)', 'rgba(0, 204, 255, 0.4)', 'rgba(204, 102, 255, 0.4)', 'rgba(128, 0, 128, 0.4)'];
     let counter = 0;
     let myChart = document.getElementById('myChart').getContext('2d');
 
@@ -260,7 +127,7 @@
                     @endforeach
                 ],
                 borderColor: '#777',
-                backgroundColor: `${color[counter ++]}`,
+                backgroundColor: `${color[counter++]}`,
                 borderWidth: 1
             },
             @endforeach
@@ -337,4 +204,12 @@
             pdf.save("{{$feedbackForm->title}}.pdf");
         });
     };
+</script>
+
+<!-- Script to make email section visible -->
+<script type="text/javascript">
+    function showElement() {
+        element = document.querySelector('.formEmail');
+        element.style.visibility = 'visible';
+    }
 </script>
