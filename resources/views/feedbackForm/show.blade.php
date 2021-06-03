e27b5e5e04371cc1f1fa4eae0629944d2c290afd
Dave Elshout <elsh0009@hz.nl>
2021-06-03 16:34:46 +0200

    average now working
    show blade now has average numbers of the questions. still need to be implemented in the PDF
    
    Co-Authored-By: Thomas Melis <70569279+ThomasMelis@users.noreply.github.com>

--- resources/views/feedbackForm/show.blade.php
+++ resources/views/feedbackForm/show.blade.php
@@ -44,6 +44,7 @@
                             @foreach($feedbackForms as $feedbackForm)
                                 <h3>{{$feedbackForm->title}}</h3>
                             @endforeach
+
                             <br>
                             <!-- PDF button -->
                             <x-button class="ml-3" onclick="getPDF()">
@@ -84,17 +85,18 @@
                                 </div>
                             </form>
 
-                            <!-- PDF section (everything in here will be in the PDF) -->
-                            <div class="canvas_div_pdf">
-                                <div class="container">
-                                    <canvas id="myChart" width="1500" height="1000"></canvas>
-                                </div>
-                                <!-- table with answer information -->
-                                <table class="table">
-                                    <thead>
-                                    <tr>
-                                        <th scope="col">Questions</th>
-                                        @foreach($feedbackForm->answerForms as $answerForm)
+
+                            <div class="container">
+                                <canvas id="myChart" width="1500" height="1000"></canvas>
+                            </div>
+                            <!-- table with answer information -->
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th scope="col">Questions</th>
+                                    @foreach($feedbackForm->answerForms as $answerForm)
+                                        @if($loop->first)
+                                            <th scope="col">Average</th>
                                             @if($answerForm->guest == NULL)
                                                 <th scope="col">{{$answerForm->user->name}}
                                                     - <br>{{$answerForm->user->role->name}}
@@ -108,114 +110,146 @@
                                                 <th scope="col">{{$answerForm->guest->name}}
                                                     - {{$answerForm->guest->role->name}} </th>
                                             @endif
-                                        @endforeach
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    @foreach($feedbackForm->questions as $question)
-                                        <div></div>
-                                        <tr>
-                                            <th scope="row">{{$question->question}}</th>
-                                            @foreach($question->answers as $answer)
-                                                <td>{{$answer->answer}}</td>
-                                            @endforeach
-                                        </tr>
+                                        @else
+                                            @if($answerForm->guest == NULL)
+                                                <th scope="col">{{$answerForm->user->name}}
+                                                    - <br>{{$answerForm->user->role->name}}
+                                                    @if($answerForm->user->role_verified)
+                                                        <div class="verified"></div>
+                                                    @else
+                                                        <div class="not_verified"> x</div>
+                                                    @endif
+                                                </th>
+                                            @else
+                                                <th scope="col">{{$answerForm->guest->name}}
+                                                    - {{$answerForm->guest->role->name}} </th>
+                                            @endif
+                                        @endif
                                     @endforeach
-                                    </tbody>
-                                </table>
-                                <a class="btn pull-right" style="border-color: #3b82f6"
-                                   href="/feedbackForm/{{$binder->public_id}}/edit">Edit</a>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($feedbackForm->questions as $question)
+                                    <div></div>
+                                    <tr>
+                                        <th scope="row">{{$question->question}}</th>
+{{--                                        @if($loop->first)--}}
+{{--                                            @foreach($feedbackFormsPDF->where('id', $feedbackForm->id) as $form)--}}
+{{--                                                @foreach($form->avg as $avg)--}}
+{{--                                                    <td>{{$avg}}</td>--}}
+{{--                                                @endforeach--}}
+{{--                                            @endforeach--}}
+{{--                                        @else--}}
+                                            @foreach($question->answers as $answer)
+                                                @if($loop->first)
+                                                    @foreach($feedbackFormsPDF->where('id', $feedbackForm->id) as $form)
+                                                    <td>{{$form->avg[$loop->parent->parent->index]}}</td>
+                                                    @endforeach
+                                                    <td>{{$answer->answer}}</td>
+                                                @else
+                                                <td>{{$answer->answer}}</td>
+                                                @endif
+                                            @endforeach
+{{--                                        @endif--}}
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                            <a class="btn pull-right" style="border-color: #3b82f6"
+                               href="/feedbackForm/{{$binder->public_id}}/edit">Edit</a>
 
-                                <form method="POST" action="{{route('feedbackForm.destroy', $binder->public_id) }}">
-                                    @method('DELETE')
-                                    @csrf
-                                    <button class="btn pull-right" type="submit"
-                                            onclick="return confirm('Are you sure you want to delete this form?')"
-                                            style="border-color: #3b82f6">Delete this form
-                                    </button>
-                                </form>
+                            <form method="POST" action="{{route('feedbackForm.destroy', $binder->public_id) }}">
+                                @method('DELETE')
+                                @csrf
+                                <button class="btn pull-right" type="submit"
+                                        onclick="return confirm('Are you sure you want to delete this form?')"
+                                        style="border-color: #3b82f6">Delete this form
+                                </button>
+                            </form>
 
-                                @else
-                                    <div class="danger">
-                                        <h3>Uh oh! <br><br></h3>
-                                        <h4>
-                                            It looks like something went wrong in the making of this form!
-                                            It is best to delete this form and try again. If the problem persists, we
-                                            fucked up.<br>
-                                        </h4>
+                            @else
+                                <div class="danger">
+                                    <h3>Uh oh! <br><br></h3>
+                                    <h4>
+                                        It looks like something went wrong in the making of this form!
+                                        It is best to delete this form and try again. If the problem persists, we
+                                        fucked up.<br>
+                                    </h4>
 
-                                        <form method="POST"
-                                              action="{{route('feedbackForm.destroy', $binder->public_id) }}">@method('DELETE') @csrf
-                                            <button class="btn pull-right" type="submit" style="border-color: #3b82f6">
-                                                Delete this form
-                                            </button>
-                                        </form>
-                                    </div>
-                                @endif
-                                @else
-                                    You don't have permission to view this Form.
-                                @endif
-                            </div>
+                                    <form method="POST"
+                                          action="{{route('feedbackForm.destroy', $binder->public_id) }}">@method('DELETE') @csrf
+                                        <button class="btn pull-right" type="submit" style="border-color: #3b82f6">
+                                            Delete this form
+                                        </button>
+                                    </form>
+                                </div>
+                            @endif
+                            @else
+                                You don't have permission to view this Form.
+                            @endif
                         </div>
                     </div>
         </div>
     </div>
 
-@foreach($feedbackFormsPDF as $form)
-    <!-- Everything inside this class will be in the PDF -->
-        <div style="width: 1200px; height: 1500px;  position: absolute;
+@if($binder->form_count == $formCount )
+
+    @foreach($feedbackFormsPDF as $form)
+        <!-- Everything inside this class will be in the PDF -->
+            <div style="width: 1200px; height: 1500px;  position: absolute;
   left:     -10000px; display: inline-block;"
-             class="canvas_div_pdf{{$form->id}}" id="clipped">
-            @if(Auth::user()->id == $binder->user_id)
-                <br>
+                 class="canvas_div_pdf{{$form->id}}" id="clipped">
+                @if(Auth::user()->id == $binder->user_id)
+                    <br>
 
-                <h1>{{$form->title}}</h1>
-                <br>
+                    <h1>{{$form->title}}</h1>
+                    <br>
 
-                <div class="container">
-                    <canvas id="myChart{{$form->id}}"
-                            style="margin-bottom: 200px; width:1110px; height:740px"></canvas>
-                </div>
+                    <div class="container">
+                        <canvas id="myChart{{$form->id}}"
+                                style="margin-bottom: 200px; width:1110px; height:740px"></canvas>
+                    </div>
 
-                <!-- table with answer information -->
-                <table class="table">
-                    <thead>
-                    <tr>
-                        <th scope="col">Questions</th>
-                        @foreach($feedbackForm->answerForms as $answerForm)
-                            @if($answerForm->guest == NULL)
-                                <th scope="col">{{$answerForm->user->name}}
-                                    - <br>{{$answerForm->user->role->name}}
-                                    @if($answerForm->user->role_verified)
-                                        <div class="verified"></div>
-                                    @else
-                                        <div class="not_verified"> x</div>
-                                    @endif
-                                </th>
-                            @else
-                                <th scope="col">{{$answerForm->guest->name}}
-                                    - {{$answerForm->guest->role->name}} </th>
-                            @endif
-                        @endforeach
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($form->questions as $question)
-                        <div></div>
+                    <!-- table with answer information -->
+                    <table class="table">
+                        <thead>
                         <tr>
-                            <th scope="row">{{$question->question}}</th>
-                            @foreach($question->answers as $answer)
-                                <td>{{$answer->answer}}</td>
+                            <th scope="col">Questions</th>
+                            @foreach($feedbackForm->answerForms as $answerForm)
+                                @if($answerForm->guest == NULL)
+                                    <th scope="col">{{$answerForm->user->name}}
+                                        - <br>{{$answerForm->user->role->name}}
+                                        @if($answerForm->user->role_verified)
+                                            <div class="verified"></div>
+                                        @else
+                                            <div class="not_verified"> x</div>
+                                        @endif
+                                    </th>
+                                @else
+                                    <th scope="col">{{$answerForm->guest->name}}
+                                        - {{$answerForm->guest->role->name}} </th>
+                                @endif
                             @endforeach
                         </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            @else
-                You don't have permission to view this Form.
-            @endif
-        </div>
-    @endforeach
+                        </thead>
+                        <tbody>
+                        @foreach($form->questions as $question)
+                            <div></div>
+                            <tr>
+                                <th scope="row">{{$question->question}}</th>
+                                @foreach($question->answers as $answer)
+                                    <td>{{$answer->answer}}</td>
+                                @endforeach
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                @else
+                    You don't have permission to view this Form.
+                @endif
+            </div>
+        @endforeach
+    @endif
 </x-app-layout>
 
 @if($binder->form_count == $formCount)
@@ -227,16 +261,40 @@
 
         const data = {
 
+            //labels are the questions shown in the form
             labels: [@foreach($feedbackForm->questions as $question)
                 '{{$question->question}}',
                 @endforeach],
+
+            //the actual data for in the chart
             datasets: [
                     @foreach($feedbackForm->answerForms as $answerForm){
+
+                    //set average in first loop, then iterate over first user
+                    @if($loop->first)
+                    label: 'average',
+
+                    data: [
+                        @foreach($feedbackFormsPDF->where('id', $feedbackForm->id) as $form)
+                            @foreach($form->avg as $avg)
+                            '{{$avg}}',
+                        @endforeach
+                        @endforeach
+
+                    ],
+                    borderColor: '#000000',
+                    backgroundColor: '#FF000000',
+                    borderWidth: 1.5
+                },
+
+                //in loop first, add first user after average has been set
+                {
                     @if($answerForm->guest == NULL)
                     label: '{{$answerForm->user->role->name}}',
                     @else
                     label: '{{$answerForm->guest->role->name}}',
                     @endif
+
                     data: [
                         @foreach($answerForm->answers as $answer)
                             '{{$answer->answer}}',
@@ -246,10 +304,41 @@
                     backgroundColor: `${color[counter++]}`,
                     borderWidth: 1
                 },
-                @endforeach
+                //after loop first add the rest of the data
+                @else
+                    @if($answerForm->guest == NULL)
+                    label
+        :
+        '{{$answerForm->user->role->name}}',
+            @else
+                label
+        :
+        '{{$answerForm->guest->role->name}}',
+            @endif
 
-            ]
-        };
+                data
+        :
+        [
+            @foreach($answerForm->answers as $answer)
+                '{{$answer->answer}}',
+            @endforeach
+        ],
+            borderColor
+        :
+        '#777',
+            backgroundColor
+        :
+        `${color[counter++]}`,
+            borderWidth
+        :
+        1
+        },
+        @endif
+        @endforeach
+
+        ]
+        }
+        ;
 
         // Global Options
         Chart.defaults.global.defaultFontFamily = 'Arial';
