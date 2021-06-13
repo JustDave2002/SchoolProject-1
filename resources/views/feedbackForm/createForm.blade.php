0ec6e307d9b44466e82da198cff0837aecd645d4
Dave Elshout <elsh0009@hz.nl>
2021-06-14 01:09:16 +0200

    Revert "Merge branch 'Dave-new' into Thomas"
    This reverts commit e92af43fdc99fdfe31431628cf5335edce5172a3, reversing
    changes made to 405c04d77f321620f473c5d74467abf8b6e1700b.

--- resources/views/feedbackForm/createForm.blade.php
+++ resources/views/feedbackForm/createForm.blade.php
@@ -22,49 +22,49 @@
                         <div class="form-group">
                             <label for="title">Title</label><br>
                             <input type="text" id="title" class="form-control" placeholder="Enter Title" name="title"
-                                   value="" required>
+                                   value="Feedback for Tom" required>
                             <div class="valid-feedback"><br></div>
                             <div class="invalid-feedback">Please fill out this field.</div>
                         </div>
                         <div class="form-group">
                             <label for="q1">Question</label><br>
                             <input type="text" id="title" class="form-control" placeholder="Question 1"
-                                   name="question[]" value="" required>
+                                   name="question[]" value="How good is Tom good at teamwork">
                             <div class="valid-feedback"><br></div>
                             <div class="invalid-feedback">Please fill out this field.</div>
                         </div>
                         <div class="form-group">
                             <label for="q2">Question</label><br>
                             <input type="text" id="title" class="form-control" placeholder="Question 2"
-                                   name="question[]" value="" required>
+                                   name="question[]" value="How good is Toms motivation" required>
                             <div class="valid-feedback"><br></div>
                             <div class="invalid-feedback">Please fill out this field.</div>
                         </div>
                         <div class="form-group">
                             <label for="q3">Question</label><br>
                             <input type="text" id="title" class="form-control" placeholder="Question 3"
-                                   name="question[]" value="" required>
+                                   name="question[]" value="How good is Toms attitude " required>
                             <div class="valid-feedback"><br></div>
                             <div class="invalid-feedback">Please fill out this field.</div>
                         </div>
                         <div class="form-group">
                             <label for="q4">Question</label><br>
                             <input type="text" id="title" class="form-control" placeholder="Question 4"
-                                   name="question[]" value="" required>
+                                   name="question[]" value="filler question" required>
                             <div class="valid-feedback"><br></div>
                             <div class="invalid-feedback">Please fill out this field.</div>
                         </div>
                         <div class="form-group">
                             <label for="q5">Question</label><br>
                             <input type="text" id="title" class="form-control" placeholder="Question 5"
-                                   name="question[]" value="" required>
+                                   name="question[]" value="filler question" required>
                             <div class="valid-feedback"><br></div>
                             <div class="invalid-feedback">Please fill out this field.</div>
                         </div>
                         <div class="form-group">
                             <label for="q6">Question</label><br>
                             <input type="text" id="title" class="form-control" placeholder="Question 6"
-                                   name="question[]" value="" required>
+                                   name="question[]" value="filler question" required>
                             <div class="valid-feedback"><br></div>
                             <div class="invalid-feedback">Please fill out this field.</div>
                         </div>
