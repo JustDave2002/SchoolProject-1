20e0ba83e65acf9e4d3c175f7eba1c43d2dcf714
Dave Elshout <elsh0009@hz.nl>
2021-06-14 13:28:40 +0200

    removed standard test values

--- resources/views/feedbackForm/createForm.blade.php
+++ resources/views/feedbackForm/createForm.blade.php
@@ -22,56 +22,55 @@
                         <div class="form-group">
                             <label for="title">Title</label><br>
                             <input type="text" id="title" class="form-control" placeholder="Enter Title" name="title"
-                                   value="Feedback for Tom" required>
+                                   required>
                             <div class="valid-feedback"><br></div>
                             <div class="invalid-feedback">Please fill out this field.</div>
                         </div>
                         <div class="form-group">
                             <label for="q1">Question</label><br>
                             <input type="text" id="title" class="form-control" placeholder="Question 1"
-                                   name="question[]" value="How good is Tom good at teamwork">
+                                   name="question[]" required>
                             <div class="valid-feedback"><br></div>
                             <div class="invalid-feedback">Please fill out this field.</div>
                         </div>
                         <div class="form-group">
                             <label for="q2">Question</label><br>
                             <input type="text" id="title" class="form-control" placeholder="Question 2"
-                                   name="question[]" value="How good is Toms motivation" required>
+                                   name="question[]" required>
                             <div class="valid-feedback"><br></div>
                             <div class="invalid-feedback">Please fill out this field.</div>
                         </div>
                         <div class="form-group">
                             <label for="q3">Question</label><br>
                             <input type="text" id="title" class="form-control" placeholder="Question 3"
-                                   name="question[]" value="How good is Toms attitude " required>
+                                   name="question[]" required>
                             <div class="valid-feedback"><br></div>
                             <div class="invalid-feedback">Please fill out this field.</div>
                         </div>
                         <div class="form-group">
                             <label for="q4">Question</label><br>
                             <input type="text" id="title" class="form-control" placeholder="Question 4"
-                                   name="question[]" value="filler question" required>
+                                   name="question[]" required>
                             <div class="valid-feedback"><br></div>
                             <div class="invalid-feedback">Please fill out this field.</div>
                         </div>
                         <div class="form-group">
                             <label for="q5">Question</label><br>
                             <input type="text" id="title" class="form-control" placeholder="Question 5"
-                                   name="question[]" value="filler question" required>
+                                   name="question[]" required>
                             <div class="valid-feedback"><br></div>
                             <div class="invalid-feedback">Please fill out this field.</div>
                         </div>
                         <div class="form-group">
                             <label for="q6">Question</label><br>
                             <input type="text" id="title" class="form-control" placeholder="Question 6"
-                                   name="question[]" value="filler question" required>
+                                   name="question[]" required>
                             <div class="valid-feedback"><br></div>
                             <div class="invalid-feedback">Please fill out this field.</div>
                         </div>
                         <br>
                         <br>
                         <input type="hidden" id="goBack" name="goBack" value="0">
-                        {{--                        {{$index}}{{$counter}}--}}
                         <div class="row">
                             <div class="col-md-6 text-left">
                                 @if($index > 0)
