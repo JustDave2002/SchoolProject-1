5633bb23c4b35d8bdcd465688a37e46c1607c575
Dave Elshout <elsh0009@hz.nl>
2021-06-16 14:20:52 +0200

    fixed bug where guests couldnt fill in feedback

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -40,9 +40,9 @@
                     <x-slot name="trigger">
                         <button
                             class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                            <div>{{ Auth::user()->name ?? 'Guest'}}  @if(Auth::user()->role_verified)
+                            <div>{{ Auth::user()->name ?? 'Guest'}}  @if(Auth::check() && Auth::user()->role_verified)
                                     <div class="verified" style="margin-top: 4px"></div>
-                                @else
+                                @elseif (Auth::check() && Auth::user()->role_verified == NULL)
                                     <div class="not_verified"style="margin-top: -1px"> x</div>
                                 @endif</div>
 
