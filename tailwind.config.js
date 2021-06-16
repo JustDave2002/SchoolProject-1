0d9e8d8cd8a44e5c5bd9a5b0bca86afa69606c11
Dave Elshout <elsh0009@hz.nl>
2021-06-16 14:20:33 +0200

    removed dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  darkMode: 'media',
+  darkMode: false,
 
     purge: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
